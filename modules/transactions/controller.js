d599c755ccc9898b77649fe2a81c1c0d09c96da9
elstr <eleonora.lester@gmail.com>
2020-04-21 19:51:01 -0300

    feat: adds controller logic

--- modules/transactions/controller.js
+++ modules/transactions/controller.js
@@ -1,11 +1,16 @@
+const { TransactionSchema } = require("./model");
+const validations = require("../../helpers/validations");
+
 const Network = require("../../helpers/network");
 const network = new Network();
 
 class TransactionsController {
   static getBalance(req, res) {
     try {
-      network.setSuccess(200, "Current balance", 100);
+      const currentBalance = TransactionSchema.getCurrentBalance();
+      network.setSuccess(200, "Current balance", currentBalance);
       return network.send(res);
+
     } catch (error) {
       network.setError(500, error.message );
       return network.send(res);
@@ -14,8 +19,12 @@ class TransactionsController {
 
   static get(req, res) {
     try {
-      network.setSuccess(200, "Transaction found", {id: 1});
+      const { id } = req.params;
+      const transaction = TransactionSchema.get(id);
+      
+      network.setSuccess(200, "Transaction found", transaction);
       return network.send(res);
+
     } catch (error) {
       network.setError(500, error.message );
       return network.send(res);
@@ -23,19 +32,39 @@ class TransactionsController {
   }
 
   static async apply(req, res) {
+    console.log("todo: lock")
     try {
-      network.setSuccess(201, "Transaction stored", {id: 1});
+      const { type, amount } = req.body;
+
+      const tranType = validations.transactionType(type);
+      if (!tranType) throw new Error("Transaction not supported");
+
+      const tranAmount = validations.transactionAmount(amount);
+      if (!tranAmount) throw new Error("Incorrect transaction amount format");
+
+      const transaction = new TransactionSchema(tranType, tranAmount);
+      transaction.apply()
+      
+      network.setSuccess(201, "Transaction stored", transaction);
       return network.send(res);
+
     } catch (error) {
       network.setError(500, error.message );
       return network.send(res);
-    } 
+    } finally {
+      // unlock
+      console.log("unlock")
+    }
+
   }
 
   static list(req, res) {
     try {
-      network.setSuccess(200, "Transactions found", [{id:1}, {id: 2}]);
+      const transactions = TransactionSchema.list();
+
+      network.setSuccess(200, "Transactions found", transactions);
       return network.send(res);
+      
     } catch (error) {
       network.setError(500, error.message );
       return network.send(res);
