f495f8a177fd160cc352eab6d19c777a4c137093
elstr <eleonora.lester@gmail.com>
2020-04-21 19:31:40 -0300

    feat: adds transaction model

---
+++ modules/transactions/model.js
@@ -0,0 +1,44 @@
+const shortid = require("shortid");
+let db = require("../../db");
+
+const TransactionType = {
+  CREDIT: "credit",
+  DEBIT: "debit",
+};
+
+class TransactionSchema {
+  constructor(type, amount) {
+    this.id = shortid.generate();
+    this.type = type;
+    this.amount = amount;
+    this.effectiveDate = new Date();
+  }
+
+  static balance = 1000;
+
+  static getCurrentBalance = () => TransactionSchema.balance;
+  static get = (id) => db.find((t) => t.id === id);
+  static save = (transaction) => (db = [...db, transaction]);
+  static list = () => db;
+
+  apply = () => {
+    switch (this.type) {
+      case TransactionType.CREDIT:
+        TransactionSchema.balance += this.amount;
+        break;
+      case TransactionType.DEBIT:
+        if (TransactionSchema.balance < this.amount)
+          throw new Error("Insuficient balance");
+        TransactionSchema.balance -= this.amount;
+        break;
+      default:
+        throw new Error("Transaction type not supported");
+    }
+    TransactionSchema.save(this)
+  };
+}
+
+module.exports = {
+  TransactionSchema,
+  TransactionType,
+};
