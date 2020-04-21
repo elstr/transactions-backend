8fd95ddbd61980979f80c289688a81b4d4b51fb2
elstr <eleonora.lester@gmail.com>
2020-04-21 18:56:26 -0300

    feat: adds network response helper

---
+++ helpers/network.js
@@ -0,0 +1,29 @@
+class Network {
+    constructor() {
+      this.statusCode = null;
+      this.type = null;
+      this.data = null;
+      this.message = null;
+    }
+  
+    setSuccess(statusCode, message, data) {
+      this.statusCode = statusCode;
+      this.message = message;
+      this.data = data;
+      this.type = "success";
+    }
+  
+    setError(statusCode, message) {
+      this.statusCode = statusCode;
+      this.message = message;
+      this.type = "error";
+    }
+  
+    send(res) {
+        return res.status(this.statusCode).json(this);
+      }
+  
+  }
+  
+  module.exports = Network;
+  
