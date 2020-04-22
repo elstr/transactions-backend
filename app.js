e9a36d4082081816d21b6a9a7a98cb7d4e2cbd63
elstr <eleonora.lester@gmail.com>
2020-04-22 17:48:17 -0300

    fix: configs for frontend to work

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ const transactions = require("./modules/transactions/routes");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/", transactions);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`🚀 Server ready at port ${port}`));
