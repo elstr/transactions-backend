e904230945ca8b5a8a509aa2dd1420612e9f5621
Eleonora Lester <eleonora.lester@gmail.com>
2020-04-26 16:46:00 -0300

    Update documentation commented

--- helpers/q.js
+++ helpers/q.js
@@ -3,9 +3,9 @@ const qrate = require("qrate");
 /**
  * All endpoints implementing queueing must return a Network object from the controller called
  * to be able to send a response.
- * See modules/transactions/controller.js apply method
  * @param {function} fn  Function to be called for each item in the queue
- * @param {function} done Function called automatically when item finished processing
+ * See modules/transactions/controller.js apply method to see network being returned
+ * See routes modules/transactions/routes.js post method to see queueing applied
  */
 const worker = async function ({ req, res, fn }) {
   const result = await fn(req);
